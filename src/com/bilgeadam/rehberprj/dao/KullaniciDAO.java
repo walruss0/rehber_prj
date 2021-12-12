42d1d99aeb2a85aa3546ec9393744034922ffec4
Onur Yildirim <onuryildirim5425@gmail.com>
2021-12-12 13:47:47 +0300

    actionPerformed'da kullanıcıadı ve şifre ile db eşleştirmesi yaptık. Birkaç validasyon ekledik

--- src/com/bilgeadam/rehberprj/dao/KullaniciDAO.java
+++ src/com/bilgeadam/rehberprj/dao/KullaniciDAO.java
@@ -10,7 +10,7 @@ public class KullaniciDAO {
     public static boolean giriseYetkiliMi(KullaniciDTO kullanici) throws SQLException, ClassNotFoundException
     {
 
-        String vtSifre ="";
+        String vtSifre =null;
 
         Connection conn = VTBaglanti.baglantiGetir();
 
--- src/com/bilgeadam/rehberprj/gui/LoginForm.java
+++ src/com/bilgeadam/rehberprj/gui/LoginForm.java
@@ -1,9 +1,13 @@
 package com.bilgeadam.rehberprj.gui;
 
+import com.bilgeadam.rehberprj.dao.KullaniciDAO;
+import com.bilgeadam.rehberprj.dto.KullaniciDTO;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 public class LoginForm extends JFrame {
     private JTextField adTF;
@@ -26,13 +30,44 @@ public class LoginForm extends JFrame {
         setTitle("Giriş Ekranı");
         setSize(300,200);
         jPanel1.setBackground(Color.black);
+        kullaniciAdiLB.setForeground(Color.red);
         setLocationRelativeTo(null);
 
 
         girisButon.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,"Butona basıldı!");
+              //  JOptionPane.showMessageDialog(null,"Butona basıldı!");
+                if (adTF.getText().length()==0)
+                {
+                    JOptionPane.showMessageDialog(null, "Ad alanı boş geçilemez");
+                    return;
+                }
+                if (String.valueOf(sifrePF.getPassword()).length()==0)
+                {
+                    JOptionPane.showMessageDialog(null,"Şifre alanı boş geçilemez");
+                    return;
+                }
+                try {
+                    KullaniciDTO kullanici = new KullaniciDTO();
+                    kullanici.setKullaniciAdi(adTF.getText());
+                    kullanici.setSifre(String.valueOf(sifrePF.getPassword()));
+
+                    boolean sonuc = KullaniciDAO.giriseYetkiliMi(kullanici);
+                    if (sonuc) JOptionPane.showMessageDialog(null,"Girişe yetkili!");
+                    else JOptionPane.showMessageDialog(null,"Girişe yetkili değil!");
+
+
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(null,"Hata:"+ex.getMessage());
+                } catch (ClassNotFoundException ex) {
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(null,"Hata:"+ex.getMessage());
+                }
+
+
+
             }
         });
     }
