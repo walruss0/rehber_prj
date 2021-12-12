846c9166cd14432985d402634290173e0f10c2b9
Onur Yildirim <onuryildirim5425@gmail.com>
2021-12-12 13:00:06 +0300

    Login page eklendi Calistir class eklendi

---
+++ src/com/bilgeadam/rehberprj/Calistir.java
@@ -0,0 +1,18 @@
+package com.bilgeadam.rehberprj;
+
+import com.bilgeadam.rehberprj.gui.LoginForm;
+
+import java.awt.*;
+
+public class Calistir {
+    public static void main(String[] args) {
+        LoginForm loginForm = new LoginForm();
+        loginForm.setVisible(true);
+
+
+
+
+
+
+    }
+}
--- src/com/bilgeadam/rehberprj/gui/LoginForm.java
+++ src/com/bilgeadam/rehberprj/gui/LoginForm.java
@@ -1,4 +1,43 @@
 package com.bilgeadam.rehberprj.gui;
 
-public class LoginForm {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginForm extends JFrame {
+    private JTextField adTF;
+    private JPasswordField sifrePF;
+    private JButton girisButon;
+    private JLabel sifreLB;
+    private JLabel kullaniciAdiLB;
+    private JPanel jPanel1;
+    private JPanel jPanel2;
+
+    public LoginForm()
+    {
+        add(jPanel1);
+
+
+
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setTitle("Giriş Ekranı");
+        setSize(300,200);
+        jPanel1.setBackground(Color.black);
+        setLocationRelativeTo(null);
+
+
+        girisButon.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Butona basıldı!");
+            }
+        });
+    }
+
+
+
+
 }
